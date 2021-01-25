2765ab9307a9999935cf63f397444816facaf824
chloekim917 <chloekim917@gmail.com>
2021-01-25 15:47:48 -0500

    controllers

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::CategoriesController < ApplicationController
+    def index
+        categories = Category.all
+        render json: categories
+    end
+
+    def update
+        category = Category.find(params[:id])
+        category.update!(category_params)
+        render json: category
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name, :budget_amount)
+    end
+
+
 end
--- app/controllers/api/v1/expenditures_controller.rb
+++ app/controllers/api/v1/expenditures_controller.rb
@@ -1,2 +1,42 @@
 class Api::V1::ExpendituresController < ApplicationController
 end
+class Api::V1::ExpendituresController < ApplicationController
+    def index
+        expenditures = Expenditure.all
+        options = {include: :category}
+        render json: ExpenditureSerializer.new(expenditures)
+    end
+
+    def by_date
+        date = params[:q]
+        expenditures = Expenditure.where(date: date)
+        render json: expenditures, include: :category
+    end
+
+    def create
+        expenditure = Expenditure.new(expenditure_params)
+        expenditure.save!
+        options = {include: :category}
+        render json: ExpenditureSerializer.new(expenditure)
+    end
+
+    def update
+        expenditure = Expenditure.find(params[:id])
+        expenditure.update!(expenditure_params)
+        options = {include: :category}
+        render json: ExpenditureSerializer.new(expenditure)
+    end
+
+    def destroy
+        expenditure = Expenditure.find(params[:id])
+        expenditure.destroy!
+        options = {include: :category}
+        render json: ExpenditureSerializer.new(expenditure)
+    end
+
+    private
+
+    def expenditure_params
+        params.require(:expenditure).permit(:date, :category_id, :detail, :amount)
+    end
+end
