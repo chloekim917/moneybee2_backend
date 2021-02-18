9e8bbcbcfa04d58e97186088a0882984d43d7af2
chloekim917 <chloekim917@gmail.com>
2021-02-18 18:35:31 -0500

    auth initial

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
       resources :categories
       resources :expenditures
       get '/expenditures_by_date', to: 'expenditures#by_date'
+      
+      # auth
+      post "/signup", to: "users#create"
+      post "/login", to: "auth#login"
+
+      get "/auto_login", to: "auth#auto_login"
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
